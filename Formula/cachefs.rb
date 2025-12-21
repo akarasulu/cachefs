3558b12c1df1b0008ebc6b2aab8000475a732e80
Alex O. Karasulu <akarasulu@apache.org>
2025-12-21 18:35:02 +0300

    fixing brew build

--- Formula/cachefs.rb
+++ Formula/cachefs.rb
@@ -1,10 +1,10 @@
 class Cachefs < Formula
   desc "FUSE caching filesystem fork of bindfs with SQLite metadata store"
   homepage "https://github.com/akarasulu/cachefs"
-  url "https://github.com/akarasulu/cachefs.git", branch: "main"
+  url "https://github.com/akarasulu/cachefs.git", branch: "master"
   version "1.0.0"
   license "GPL-2.0-or-later"
-  head "https://github.com/akarasulu/cachefs.git", branch: "main"
+  head "https://github.com/akarasulu/cachefs.git", branch: "master"
 
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
@@ -15,15 +15,38 @@ class Cachefs < Formula
     depends_on "fuse3"
   end
 
-  on_macos do
-    depends_on macfuse: :cask
-  end
-
   depends_on "sqlite"
 
   def install
+    macfuse_prefix = HOMEBREW_PREFIX
+
+    if OS.mac?
+      # macFUSE (cask) does not ship pkg-config files; generate shims so configure can find it.
+      pcdir = buildpath/"pkgconfig"
+      pcdir.mkpath
+      %w[fuse fuse3].each do |name|
+        (pcdir/"#{name}.pc").write <<~EOS
+          prefix=#{macfuse_prefix}
+          exec_prefix=${prefix}
+          libdir=${exec_prefix}/lib
+          includedir=${prefix}/include
+          osxfuse_incdir=${prefix}/include/osxfuse
+
+          Name: #{name}
+          Description: macFUSE userspace library
+          Version: 3.11.0
+          Libs: -L${libdir} -losxfuse -pthread
+          Cflags: -I${includedir} -I${osxfuse_incdir}
+        EOS
+      end
+      ENV.prepend_path "PKG_CONFIG_PATH", pcdir
+    end
+
     system "./autogen.sh"
-    system "./configure", "--prefix=#{prefix}"
+    args = ["--prefix=#{prefix}"]
+    # Prefer the FUSE2-compatible API on macOS; fuse3 pkg-config is not available from the macFUSE cask.
+    args += ["--with-fuse3=no", "--with-fuse2=yes", "--with-fuse_t=no"] if OS.mac?
+    system "./configure", *args
     system "make"
     system "make", "install"
   end

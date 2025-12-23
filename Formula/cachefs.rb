99b8ca6696f9601a34fb96b1f23746a9c325afef
Alex O. Karasulu <aok@optdyn.com>
2025-12-23 13:32:32 +0300

    Adding the proper root url for tap to work correctly

--- Formula/cachefs.rb
+++ Formula/cachefs.rb
@@ -98,7 +98,7 @@ class Cachefs < Formula
   end
 
   bottle do
-    root_url "file:///Users/aok/cachefs/dist/bottles"
+    root_url "https://github.com/akarasulu/cachefs/blob/master/dist/bottles/cachefs--1.0.0.monterey.bottle.1.tar.gz?raw=true"
     rebuild 1
     sha256 cellar: :any, monterey: "f87e68ae0be2e862767b103be117d9eb8ddc46f3e76ebd71aeab2e4cf361e2e3"
   end
